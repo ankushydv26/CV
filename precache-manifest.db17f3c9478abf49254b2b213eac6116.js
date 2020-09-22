304a441cc3ee31866fbef31ed9176c8b1fc11e01
ankushydv26 <tankush027@gmail.com>
2020-09-23 02:57:23 +0530

    Updates

---
+++ precache-manifest.db17f3c9478abf49254b2b213eac6116.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad251be8dc1f68d32cec99de39a8187",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "31e34bf5cabdd6f6fd2d",
+    "url": "/CV/static/css/main.ac9315d8.chunk.css"
+  },
+  {
+    "revision": "5520c2d045f31ea96d33",
+    "url": "/CV/static/js/2.cd9b2f80.chunk.js"
+  },
+  {
+    "revision": "659df5b420806b55437f4f9e57f60411",
+    "url": "/CV/static/js/2.cd9b2f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e34bf5cabdd6f6fd2d",
+    "url": "/CV/static/js/main.325a3e4e.chunk.js"
+  },
+  {
+    "revision": "47b777b41f7bb949979f",
+    "url": "/CV/static/js/runtime-main.fbe74ee8.js"
+  },
+  {
+    "revision": "d1ac35895ee9029b5218f669ea9f3400",
+    "url": "/CV/static/media/facebook.d1ac3589.svg"
+  },
+  {
+    "revision": "907d4aab78cf82e6f53b25c82594b50b",
+    "url": "/CV/static/media/files-and-folders.907d4aab.svg"
+  },
+  {
+    "revision": "eb038977db4d694f38b2b2a70133333e",
+    "url": "/CV/static/media/github.eb038977.svg"
+  },
+  {
+    "revision": "59b0c8cb832538da82cba3147939d41d",
+    "url": "/CV/static/media/instagram-sketched.59b0c8cb.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CV/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91178edd2b36fa7f399a359a25068c96",
+    "url": "/CV/static/media/shanuCrop.91178edd.jpg"
+  }
+]);
